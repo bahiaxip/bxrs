3a93c354a29b62093ab3602245043bb1aafe040f
xip <mundaxip@gmal.com>
2021-06-28 15:43:52 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionicRRSS',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
